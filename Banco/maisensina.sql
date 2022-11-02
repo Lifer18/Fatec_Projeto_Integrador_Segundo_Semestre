98dd98ba9b2505457d75328823c1ff6c4d8d4cab
Fernando Maldonado Fernandes <fernando-maldonado@hotmail.com>
2022-11-01 21:46:52 -0300

    Sprints ate o momento do P.i - Banco e Documentação

---
+++ Banco/maisensina.sql
@@ -0,0 +1,41 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.24-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Copiando dados para a tabela maisensina.administrativo: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.aluno: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.cargo: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.certificado: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.emissao: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.empresa: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.escola: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.post: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.professor: ~0 rows (aproximadamente)
+
+-- Copiando dados para a tabela maisensina.turma: ~0 rows (aproximadamente)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
